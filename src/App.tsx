cbb4eef91815056737215dc6b4266d6b6fcd6a36
Taynara Pecorario <taynara_rp22@hotmail.com>
2021-11-10 14:39:51 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from "@pages/Login";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
